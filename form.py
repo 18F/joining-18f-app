02709220d8fbc03afca79ab220991700c3f59fcf
arowla <alison.rowland@gsa.gov>
2015-09-24 13:06:10 -0400

    Fixed form labels

--- form.py
+++ form.py
@@ -5,14 +5,14 @@ from wtforms.validators import DataRequired, Email, URL
 from werkzeug import secure_filename
 
 class ApplicationForm(Form):
-    name = StringField('name', validators=[DataRequired()])
-    email = StringField('email', validators=[DataRequired(),Email()])
-    phone = StringField('phone')
-    city = StringField('city')
-    state = StringField('state')
-    zipcode = StringField('zipcode')
-    citizenship = BooleanField('citizenship')
-    primary_skillset = SelectField('primary_skillset', choices=[
+    name = StringField('Name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(),Email()])
+    phone = StringField('Phone')
+    city = StringField('City')
+    state = StringField('State')
+    zipcode = StringField('Zip Code')
+    citizenship = BooleanField('Verify you are a U.S. Citizen or Green Card holder')
+    primary_skillset = SelectField('Please indicate your primary skillset. Your response here will be used primarily in assigning reviewers for your application.', choices=[
            ('acquisition_specialist', 'Acquisition Specialist'),
            ('back_end_developer', 'Back end developer'),
            ('content_strategist', 'Content Strategist'),
@@ -28,14 +28,14 @@ class ApplicationForm(Form):
            ('technical_recruiter', 'Technical Recruiter'),
            ('visual_designer', 'Visual Designer'),
         ])
-    other_skills = TextAreaField('other_skills')
-    link1 = StringField('link1', validators=[URL()])
-    link2 = StringField('link2', validators=[URL()])
-    link3 = StringField('link3', validators=[URL()])
-    why_join_18f = TextAreaField('why_join_18f')
-    resume = TextAreaField('resume')
-    fulltime_ok = BooleanField('fulltime_ok')
-    location_pref = SelectField('location_pref', choices=[
+    other_skills = TextAreaField('Other skills you have')
+    link1 = StringField('Link 1', validators=[URL()])
+    link2 = StringField('Link 2', validators=[URL()])
+    link3 = StringField('Link 3', validators=[URL()])
+    why_join_18f = TextAreaField('Why do you want to join 18F?')
+    resume = TextAreaField('Resume')
+    fulltime_ok = BooleanField('Verify your ability to work full-time')
+    location_pref = SelectField('Select your preferred work location', choices=[
             ('chi', 'Chicago'),
             ('ny', 'New York'),
             ('sf', 'San Francisco'),
@@ -44,7 +44,7 @@ class ApplicationForm(Form):
         ])
     is_veteran = RadioField('Are you a Veteran of the U.S. Armed Forces or are you eligible for "derived" preference?',
             choices=[('', 'No'), ('yes', 'Yes')])
-    veteran_pref_type = RadioField('veteran_pref_type', choices=[
+    veteran_pref_type = RadioField('', choices=[
             ('none', 'No, I do not claim Veterans\' Preference.'),
             ('ssp', '0-point Sole Survivorship Preference (SSP).'),
             ('tp', '5-point preference based on active duty in the U.S. Armed Forces (TP).'),
@@ -53,7 +53,7 @@ class ApplicationForm(Form):
             ('xp', '10-point preference for non-compensable disability or Purple Heart (XP).'),
             ('xp2', '10-point preference based on widow/widower or mother of a deceased veteran, or spouse or mother of a disabled veteran (XP).'),
         ])
-    addl_opps = BooleanField('addl_opps')
-    where_they_found_us = TextAreaField('where_they_found_us')
+    addl_opps = BooleanField('Please also consider me for other employment opportunities within the US government.')
+    where_they_found_us = TextAreaField('Where did you learn about 18F?')
 
 
