d762b0e48ba99ba6e19407d8b01d89c6e36eaec9
Fureigh <rhys.fureigh@gsa.gov>
2015-09-25 01:43:26 -0700

    Tweak line spacing per PEP-8

--- run.py
+++ run.py
@@ -5,12 +5,14 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 app.config.from_envvar('JOINING_18F_APP_SETTINGS')
 
+
 @app.route('/', methods=('GET', 'POST'))
 def apply():
     form = ApplicationForm()
     if form.validate_on_submit():
         return redirect('/success')
     return render_template('form.html', form=form)
+
 
 @app.route('/success')
 def success():
