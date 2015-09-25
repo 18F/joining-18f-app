04b2ba5fae361a2cb87d2a9d4eed09f76b12d879
Fureigh <rhys.fureigh@gsa.gov>
2015-09-25 01:50:06 -0700

    Add scrappy initial form-to-Google-Doc processing

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from form import ApplicationForm
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -9,7 +11,11 @@ app.config.from_envvar('JOINING_18F_APP_SETTINGS')
 @app.route('/', methods=('GET', 'POST'))
 def apply():
     form = ApplicationForm()
-    if form.validate_on_submit():
+#    if form.validate_on_submit():  # This never gets reached.
+    if request.method == 'POST':    # Temporarily forcing this until validation works.
+        application_content = stringify_form_submission(form)
+        title = form['name'].data
+        create_google_doc(title, application_content)
         return redirect('/success')
     return render_template('form.html', form=form)
 
@@ -17,6 +23,27 @@ def apply():
 @app.route('/success')
 def success():
     return render_template('success.html')
+
+
+def stringify_form_submission(form):
+    stringified_form_submission = ''
+
+    for fieldname, value in form.data.items():
+        stringified_form_submission += '{}: {}\n'.format(fieldname, value)
+
+    return stringified_form_submission
+
+
+def create_google_doc(title, application_content):
+    gauth = GoogleAuth()
+    gauth.LocalWebserverAuth()
+    drive = GoogleDrive(gauth)
+
+    test_file = drive.CreateFile({'title': '{}.txt'.format(title)})
+    test_file.SetContentString(application_content)
+    test_file.Upload()
+
+    return None
 
 if __name__ == '__main__':
     app.run(debug=True)
